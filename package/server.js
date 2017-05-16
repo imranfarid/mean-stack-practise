6acf9b6bbc91678c67c1a19021e8b60f545647a4
imranfarid7890 <imran.cyborg@gmail.com>
2017-05-16 10:48:01 +0600

    Creating posts via API

--- package/server.js
+++ package/server.js
@@ -13,6 +13,13 @@ app.get('/api/posts', function(req, res){
     ])
 })
 
+app.post('/api/posts', function(req, res){
+    console.log('post received!')
+    console.log('req.body.username')
+    console.log('req.body.body')
+    res.send(201)
+})
+
 app.listen(3000, function(){
     console.log('Server listening on', 3000)
 })
