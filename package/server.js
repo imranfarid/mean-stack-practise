a822bfeeb440715486d231ed820b2e1be976f060
imranfarid7890 <imran.cyborg@gmail.com>
2017-05-17 20:08:57 +0600

    minor changes

--- package/server.js
+++ package/server.js
@@ -1,5 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
+var Post = require('./models/post')
 
 var app = express()
 app.use(bodyParser.json())
@@ -13,11 +14,24 @@ app.get('/api/posts', function(req, res){
     ])
 })
 
+/*
 app.post('/api/posts', function(req, res){
     console.log('post received!')
     console.log('req.body.username')
     console.log('req.body.body')
     res.send(201)
+})
+*/
+
+app.post('/api/posts', function(req, res, next){
+    var post = new Post({
+        username: req.body.username,
+        body: req.body.body
+    })
+    post.save(function(err, post){
+        if(err){ return next(err) }
+        res.json(201, post)
+    })
 })
 
 app.listen(3000, function(){
