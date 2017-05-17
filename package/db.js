efc32c5dc8200cee421e183b4e122e0b0cd99091
imranfarid7890 <imran.cyborg@gmail.com>
2017-05-17 21:57:48 +0600

    CH-5 Reading posts from the API with $http

--- package/db.js
+++ package/db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/social', function(){
-    console.log('mongodb connected')
+mongoose.connect('mongodb://localhost/social', function () {
+  console.log('mongodb connected')
 })
 module.exports = mongoose
--- package/server.js
+++ package/server.js
@@ -23,15 +23,15 @@ app.post('/api/posts', function(req, res){
 })
 */
 
-app.post('/api/posts', function(req, res, next){
-    var post = new Post({
-        username: req.body.username,
-        body: req.body.body
-    })
-    post.save(function(err, post){
-        if(err){ return next(err) }
-        res.json(201, post)
-    })
+app.post('/api/posts', function (req, res, next) {
+  var post = new Post({
+    username: req.body.username,
+    body:     req.body.body,
+  })
+  post.save(function (err, post) {
+    if (err) { return next(err) }
+    res.status(201).json(post)
+  })
 })
 
 app.listen(3000, function(){
